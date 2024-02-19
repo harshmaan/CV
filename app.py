c7ad3d230cd0fd17c6642653658b9b7613c03467
Harsh Maan <32389454+harshmaan@users.noreply.github.com>
2024-02-19 10:54:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-import streamlit as st
+
+import streamlit as st 
+from constant import *
 import numpy as np 
 import pandas as pd
 from PIL import Image
@@ -7,12 +9,16 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import requests
 import re
+import plotly.graph_objects as go
 import io
 import matplotlib.pyplot as plt
 import streamlit.components.v1 as components
-import tensorflow as tf
+from graph_builder import *
+#import tensorflow as tf
+from streamlit_player import st_player
 
 st.sidebar.caption('Wish to connect?')
-st.sidebar.write('📧: maanharsh98@hotmail.com')
-pdfFileObj = open('abc.pdf', 'rb')
-st.sidebar.download_button('text_to_display',pdfFileObj,file_name='abc.pdf',mime='pdf')
+st.sidebar.write('📧: maanharsh98@hotmail')
+
+pdfFileObj = open('pdfs/CV - Harsh - 2014.pdf', 'rb')
+st.sidebar.download_button('download resume',pdfFileObj,file_name='CV - Harsh - 2014.pdf',mime='pdf')
